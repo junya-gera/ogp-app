054668ee7ffe8705712d426c363ef89cf5e42ac1
Junya WADA <junya-wada@msen.jp>
2021-05-02 10:47:38 +0900

    store アクションにメッセージと画像のファイルパスを登録する処理を追加

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -3,11 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Models\Message;
+use Illuminate\Support\Str;
 
 class MessageController extends Controller
 {
     public function store(Request $request) {
+        $params = $request->json()->all();
+        $content = $params['message'];
+        // ランダムで一意な識別子を $id に代入
+        $id = Str::uuid();
 
+        // id + .jpg を画像のファイル名とする
+        $file = $id->toString() . '.jpg';
+        Message::create([
+            'id' => $id,
+            'content' => $content,
+            'file_path' => $file,
+        ]);
+
+        return response()->json($id);
     }
 }
 
